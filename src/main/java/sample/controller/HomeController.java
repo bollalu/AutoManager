ef2faee19ccf755b3bf950739bfdccf2349e02e7
Luca <luca@bolla.name>
2016-01-04 16:16:16 +0100

    Home user

--- src/main/java/sample/controller/HomeController.java
+++ src/main/java/sample/controller/HomeController.java
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 public class HomeController {
 	
-
-	
 	//@PreAuthorize("hasAuthority('USER')")
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {		
 		return "/login";
+	}
+	
+	//@PreAuthorize("hasAuthority('USER')")
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String userHome(Model model) {
+		return "user@home";
 	}
 	
 }
--- src/main/java/sample/controller/RifornimentoController.java
+++ src/main/java/sample/controller/RifornimentoController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RifornimentoController {
 	
 	//@PreAuthorize("hasAuthority('USER')")
-	@RequestMapping(value = "/user", method = RequestMethod.GET)
-	public String home(Model model) {
-		return "user@Home";
+	@RequestMapping(value = "/user/rifornimento", method = RequestMethod.GET)
+	public String rifornimento(Model model) {
+		return "user@rifornimento";
 	}
 }
