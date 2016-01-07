d66d0e9bf36a6e71c2d4cca5fbff3f01f514c159
Luca <luca@bolla.name>
2016-01-07 21:56:05 +0100

    Modello DB cambioGomme

---
+++ src/main/java/sample/model/CambioGomme.java
@@ -0,0 +1,83 @@
+package sample.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class CambioGomme {
+	public String getDescrizione() {
+		return Descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		Descrizione = descrizione;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Veicolo getVeicolo() {
+		return veicolo;
+	}
+
+	public void setVeicolo(Veicolo veicolo) {
+		this.veicolo = veicolo;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Integer getKm() {
+		return km;
+	}
+
+	public void setKm(Integer km) {
+		this.km = km;
+	}
+
+	public Float getCosto() {
+		return costo;
+	}
+
+	public void setCosto(Float costo) {
+		this.costo = costo;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+	
+	@ManyToOne
+	@JoinColumn(name="veicolo")
+	private Veicolo veicolo;
+	
+	@Column
+    private Date data;
+	
+	@Column
+    private Integer km;
+	
+	@Column
+    private Float costo;
+	
+	@Column
+    private String Descrizione;
+
+}
---
+++ src/main/java/sample/repo/CambioGommeRepository.java
@@ -0,0 +1,15 @@
+package sample.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import sample.model.CambioGomme;
+import sample.model.Rifornimento;
+
+@Repository
+public interface CambioGommeRepository extends CrudRepository<Rifornimento, Long> {
+
+	public Iterable<CambioGomme> findCambioGommeById(@Param("id") long id);
+
+}
